3b35b6c76c6d1fe68061c998e09ed547dbb16b4f
Andrey Zharkikh <orbital.logistics@yandex.ru>
2024-01-19 08:29:18 +0300

    feat: 03-commonjs-modules

---
+++ 03-commonjs-modules/index.js
@@ -0,0 +1,7 @@
+// console.log(arguments.callee.toString());
+// console.log('Hello');
+// console.log(exports);
+// console.log(require);
+// console.log(module);
+// console.log(__filename);
+// console.log(__dirname);
